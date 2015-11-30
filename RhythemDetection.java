883cc863a8f4b9f6989dbdfed52d263d9c2df6dc
Terrence Lewis <tlewis@gwmail.gwu.edu>
2015-11-29 20:34:24 -0500

    Beginning of finding the rhythm of speakers by finding wpm
    Program detects words per min and detects differences between words to
    add to user error report

---
+++ RhythemDetection.java
@@ -0,0 +1,82 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+import edu.cmu.sphinx.result.WordResult;
+
+/**
+ * @author Terrence Lewis 
+ * Using CMU's Sphinx library for transcribing
+ * Class is designed to be the beginning of the
+ * rhythm detection - this is the basics and 
+ * attempts to find words per minute  
+ * 
+ * http://cmusphinx.sourceforge.net/wiki/tutorialsphinx4
+ */
+public class RhythemDetection {       
+                         
+    public static void main(String[] args) throws Exception {
+    	
+    	int uerCnt = 0; 
+        //used to supply required and optional attributes to recognizer                  
+        Configuration configuration = new Configuration();
+
+        configuration
+                .setAcousticModelPath("resources/edu/cmu/sphinx/models/en-us/en-us");
+        configuration
+                .setDictionaryPath("resources/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration
+                .setLanguageModelPath("resources/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+     
+        
+    /*
+     //gets live speech data 
+     //should note that it is not accurate using it just for word detection and time signatures  
+     LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+     // Start recognition process pruning previously cached data.
+     recognizer.startRecognition(true);
+     SpeechResult result = recognizer.getResult();
+     // Pause recognition process. It can be resumed then with startRecognition(false).
+    // recognizer.stopRecognition();
+     
+     while ((result = recognizer.getResult()) != null) {
+         System.out.format("#### New Hypothesis: %s\n", result.getHypothesis());
+         
+      // Get individual words and their times.
+         for (WordResult r : result.getWords()) {
+             System.out.println("t stamp: " + r);
+         }
+     }
+     recognizer.stopRecognition();
+     
+     */ 
+     
+        //used this portion for testing: as this is a file it allows for consistency with results
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(
+                configuration);
+        InputStream stream = new FileInputStream(new File("rec_10s.wav"));
+
+        recognizer.startRecognition(stream);
+        SpeechResult result = null;
+        WordResult s = null; 
+         
+        
+        while ((result = recognizer.getResult()) != null) {
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+          
+            for (WordResult r : result.getWords()) {
+                System.out.println("t stamp: " + r.getTimeFrame().getEnd()); //gets that actual time stamps of "words"
+                s = r; 
+              //UER
+                if((s.getTimeFrame().getEnd()-s.getTimeFrame().getEnd())>1600){
+                	uerCnt++; 
+                }
+            }    
+        }
+        recognizer.stopRecognition();     
+    }
+}
